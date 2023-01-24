32f8a6416a9fd4942c5c06db96e2238f9034d8e4
workflow <4farlion@gmail.com>
2023-01-24 20:35:40 +0000

    feat: add support for using flakes from devShell

--- flake.nix
+++ flake.nix
@@ -7,5 +7,9 @@
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
+    packages.x86_64-linux.devShell = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [ self.packages.x86_64-linux.figlet ];
+    };
+
   };
 }
