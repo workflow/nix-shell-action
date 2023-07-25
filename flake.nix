b1c830a7731531036864c71c536ee1b7e5d98cd3
luchiniatwork <tiago@luchini.nyc>
2023-07-25 15:28:32 -0400

    fix: remove named devShell dummy test
    The feature didn't work nor was clear

--- flake.nix
+++ flake.nix
@@ -7,11 +7,9 @@
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-    devShells.x86_64-linux.named-shell = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
       buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.figlet ];
     };
-
-    devShells.x86_64-linux.default = self.devShells.x86_64-linux.named-shell;
 
   };
 }
